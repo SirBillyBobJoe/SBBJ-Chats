887f4b3a481de684509668d39ca19d6f5cf7d80c
Kay Tang <126426690+ktan185@users.noreply.github.com>
2024-05-10 22:52:36 +1200

    chore: formatted files and removed node_modules

--- server/models/chat.js
+++ server/models/chat.js
@@ -1,40 +1,38 @@
-import mongoose from 'mongoose';
-mongoose.set('strictQuery', false);
+import mongoose from 'mongoose'
+mongoose.set('strictQuery', false)
 
+const url =
+  'mongodb+srv://SirBillyBobJoe:hoanh2005@cluster0.8ts8the.mongodb.net/chats?retryWrites=true&w=majority'
+console.log('connecting to', url)
 
-const url = "mongodb+srv://SirBillyBobJoe:hoanh2005@cluster0.8ts8the.mongodb.net/chats?retryWrites=true&w=majority";
-console.log('connecting to', url);
-
-mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB');
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message);
-    });
-
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const messageSchema = new mongoose.Schema({
-    name:String,
-    time: Date,
-    text: String,
-
-});
-
+  name: String,
+  time: Date,
+  text: String,
+})
 
 const chatSchema = new mongoose.Schema({
-    name: String,
-    messages: [messageSchema] ,
-    owner:String
-});
+  name: String,
+  messages: [messageSchema],
+  owner: String,
+})
 
 chatSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString();
-        delete returnedObject._id;
-        delete returnedObject.__v;
-    }
-});
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
 
-export const Chat = mongoose.model('Chat', chatSchema);
-export const Message = mongoose.model('Message', messageSchema);
+export const Chat = mongoose.model('Chat', chatSchema)
+export const Message = mongoose.model('Message', messageSchema)
